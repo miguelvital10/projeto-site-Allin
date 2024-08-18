e63162c0006bff104522876eb6f5b59986014aa6
Miguel Vital <miguelvital05@gmail.com>
2024-08-18 01:34:25 -0300

    Adição da estrutura do projeto e cabeçalho

---
+++ comuns/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+    <link rel="stylesheet" type="text/css" href="../recursos/css/main.css">
+    <title>All in - Treinamentos</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="conteudo">
+                <div class="logo"><img src="recursos/imagens/logo.png" alt="Logo"></div>
+                <div class="menu-lateral">
+                    <ul class="nav">
+                        <li>
+                            <ahref="#">Quem sou eu</a>
+                        </li>
+                        <li>
+                            <ahref="#">Eventos</a>
+                        </li>
+                        <li>
+                            <ahref="#">Cursos</a>
+                        </li>
+                        <li>
+                            <ahref="#">Vídeos</a>
+                        </li>
+                        <li>
+                            <ahref="#">Fale Comigo</a>
+                        </li>
+                    </ul>
+                    <div class="redes-sociais">
+                        <p>Siga-me:</p>
+                        <a href="https://www.instagram.com/miguel.vittal/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="https://www.instagram.com/miguel.vittal/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="https://w.app/MiguelArthur" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('../projeto-site-Allin/comuns/header.php');
+    require_once('../projeto-site-Allin/views/home.php');
+    require_once('../projeto-site-Allin/comuns/footer.php');
+?>
+    
