a8c802889b5bc9e67b59582b4cf32ec48045868c
Miguel Vital <miguelvital05@gmail.com>
2024-12-24 15:38:48 -0300

    implementando jquery

--- comuns/header.php
+++ comuns/header.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="../recursos/js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="../../projeto-site-Allin/recursos/css/main.css">
     <link rel="shortcut icon" type="image/jpg" href="../../projeto-site-Allin/recursos/imagens/favicon.jpg"/>
     <title>All in - Treinamentos</title>
---
+++ recursos/js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    alert("JQUERY ESTÁ FUNCIONANDO");
+});
