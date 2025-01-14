a02dd409240b401322ad6593ce4de0454d544b12
Miguel Vital <miguelvital05@gmail.com>
2025-01-14 15:44:16 -0300

    content videos

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 <main>
     <section class="quem-sou-eu">
         <div class="container">
-           <div class="conteudo">
+            <div class="conteudo">
                 <div class="img">
                     <img src="../recursos/imagens/pessoa-banner.png" alt="">
                 </div>
@@ -9,20 +9,20 @@
                     <h2>Quem sou eu?</h2>
                     <div class="descricao">
                         <p>
-                            Sou um empresário apaixonado por ajudar outros empreendedores a alcançarem o sucesso. 
-                            Ao longo dos anos, acumulei vasta experiência no gerenciamento de empresas e enfrentei muitos dos desafios que todo gestor encontra ao longo da jornada. 
+                            Sou um empresário apaixonado por ajudar outros empreendedores a alcançarem o sucesso.
+                            Ao longo dos anos, acumulei vasta experiência no gerenciamento de empresas e enfrentei muitos dos desafios que todo gestor encontra ao longo da jornada.
                             Meu objetivo é compartilhar esse conhecimento com empresários que buscam melhorar a gestão de seus negócios, otimizando processos,
                             aumentando a eficiência e tomando decisões mais assertivas. Acredito que, com as ferramentas certas e a mentalidade adequada,
                             qualquer empresa pode prosperar.
                         </p>
                         <p>
-                            Seja você um empreendedor iniciante ou alguém que já está no mercado há algum tempo, 
+                            Seja você um empreendedor iniciante ou alguém que já está no mercado há algum tempo,
                             estou aqui para ajudar a elevar sua empresa a um novo patamar.
                         </p>
                     </div>
                     <a class="btn" href="#" target="_blank">Veja o vídeo</a>
                 </div>
-           </div>
+            </div>
         </div>
     </section>
     <section class="eventos">
@@ -35,15 +35,15 @@
                             <h3>Título do evento 1 vem aqui</h3>
                             <div class="descricao">
                                 <p>
-                                Neste evento presencial exclusivo para empresários e gestores, você aprenderá técnicas e estratégias práticas para aprimorar a gestão da sua empresa. 
-                                Abordaremos desde os principais desafios do mercado atual até soluções inovadoras para otimizar processos, reduzir custos e aumentar a rentabilidade.
+                                    Neste evento presencial exclusivo para empresários e gestores, você aprenderá técnicas e estratégias práticas para aprimorar a gestão da sua empresa.
+                                    Abordaremos desde os principais desafios do mercado atual até soluções inovadoras para otimizar processos, reduzir custos e aumentar a rentabilidade.
                                 </p>
                                 <p>
-                                Além das palestras, haverá sessões de networking, 
-                                onde você poderá trocar experiências e construir parcerias com outros empresários de diversos setores.
+                                    Além das palestras, haverá sessões de networking,
+                                    onde você poderá trocar experiências e construir parcerias com outros empresários de diversos setores.
                                 </p>
                                 <p>
-                                Participe de um evento transformador e esteja preparado para levar sua empresa ao próximo nível!
+                                    Participe de um evento transformador e esteja preparado para levar sua empresa ao próximo nível!
                                 </p>
                             </div>
                         </div>
@@ -56,15 +56,15 @@
                             <h3>Título do evento 1 vem aqui</h3>
                             <div class="descricao">
                                 <p>
-                                Neste evento presencial exclusivo para empresários e gestores, você aprenderá técnicas e estratégias práticas para aprimorar a gestão da sua empresa. 
-                                Abordaremos desde os principais desafios do mercado atual até soluções inovadoras para otimizar processos, reduzir custos e aumentar a rentabilidade.
+                                    Neste evento presencial exclusivo para empresários e gestores, você aprenderá técnicas e estratégias práticas para aprimorar a gestão da sua empresa.
+                                    Abordaremos desde os principais desafios do mercado atual até soluções inovadoras para otimizar processos, reduzir custos e aumentar a rentabilidade.
                                 </p>
                                 <p>
-                                Além das palestras, haverá sessões de networking, 
-                                onde você poderá trocar experiências e construir parcerias com outros empresários de diversos setores.
+                                    Além das palestras, haverá sessões de networking,
+                                    onde você poderá trocar experiências e construir parcerias com outros empresários de diversos setores.
                                 </p>
                                 <p>
-                                Participe de um evento transformador e esteja preparado para levar sua empresa ao próximo nível!
+                                    Participe de um evento transformador e esteja preparado para levar sua empresa ao próximo nível!
                                 </p>
                             </div>
                         </div>
@@ -93,16 +93,121 @@
                         <img src="../recursos/imagens/curso-pnl.png">
                     </div>
                     <h3 class="titulo">Gestão de Empresas</h3>
-                </div><div class="item">
+                </div>
+                <div class="item">
                     <div class="img">
                         <img src="../recursos/imagens/curso-pnl.png">
                     </div>
                     <h3 class="titulo">Saúde Financeira</h3>
-                </div><div class="item">
+                </div>
+                <div class="item">
                     <div class="img">
                         <img src="../recursos/imagens/curso-pnl.png">
                     </div>
                     <h3 class="titulo">Recursos Humanos</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="videos">
+        <div class="container">
+            <div class="conteudo">
+                <h2 class="titulo_principal black">Vídeos</h2>
+                <div class="itens">
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                    <div class="item">
+                        <div class="img">
+                            <img src="../recursos/imagens/video-youtube.png">
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <h3 class="titulo">3 Técnicas Poderosas de PNL Para Você Usar Agora</h3>
+                    </div>
+                </div>
+                <div class="botao">
+                    <a href="#" class="btn" target="_blank">INSCREVA-SE</a>
                 </div>
             </div>
         </div>
